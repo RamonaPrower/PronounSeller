fed16210a856baa45729211b562fb8badbf5c033
Ramona Prower <garfieldfan3993@gmail.com>
2020-03-14 20:46:25 +0000

    multiple pronouns now

--- commands/message/pronounsArgs.js
+++ commands/message/pronounsArgs.js
@@ -5,16 +5,19 @@ module.exports = {
 	async execute(message) {
 		const splitArgs = message.content.split(' ');
 		splitArgs.shift();
-		const pronoun = splitArgs.join();
-		const details = await roleManager.updateRoles(pronoun.toLowerCase(), message.author.id, message.guild);
-		if (details.toggle === 'added') {
-			const roleMessage = `${message.author.username}, You have purchased the strongest of my ${details.tag} Pronouns.`;
-			await message.channel.send(roleMessage);
+		const pronouns = splitArgs;
+		for (const string of pronouns) {
+			const details = await roleManager.updateRoles(string.toLowerCase(), message.author.id, message.guild);
+			if (details.toggle === 'added') {
+				const roleMessage = `${message.author.username}, You have purchased the strongest of my ${details.tag} Pronouns.`;
+				await message.channel.send(roleMessage);
+			}
+			else {
+				const roleMessage = `The ${details.tag} pronouns were too strong for ${message.author.username}.`;
+				await message.channel.send(roleMessage);
+			}
 		}
-		else {
-			const roleMessage = `The ${details.tag} pronouns were too strong for ${message.author.username}.`;
-			await message.channel.send(roleMessage);
-		}
+
 	},
 };
 
